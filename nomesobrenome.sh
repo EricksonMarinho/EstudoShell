2f1c7527ec373552a07c6472f2a996e1b292130d
ericksonmarinho <ericksonmarinho33@gmail.com>
2017-10-23 12:19:18 -0200

    Primeiros comandos em Shell

---
+++ nomesobrenome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Programa que pede o nome e o sobrenome
+clear
+echo "Programa de boas vindas ao usuario"
+echo "-------------------------------------------------------"
+echo "Digite seu primeiro nome"
+read nome
+echo "Agore digite o seu sobrenome"
+read sobrenome
+echo "Ola, Sr(a) $nome $sobrenome. Hoje eh: `date`"
+
